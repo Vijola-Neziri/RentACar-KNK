0ba02269aeba8dcc8d3d4004239cbf81f8a23ddd
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-05-05 02:20:39 +0200

    login

--- src/main/java/Controllers/LoginController.java
+++ src/main/java/Controllers/LoginController.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginViewController implements Initializable {
+public class LoginController implements Initializable {
     @FXML
     private Button login;
 
