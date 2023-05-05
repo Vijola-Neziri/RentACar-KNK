f4235c45d3319aaabf25de62a454b7115e0defce
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-05-05 02:19:12 +0200

    login

--- src/main/java/Controllers/LoginViewController.java
+++ src/main/java/Controllers/LoginViewController.java
@@ -3,14 +3,19 @@ package Controllers;
 import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -50,6 +55,17 @@ public class LoginViewController implements Initializable {
             System.out.print("Login Succesfully");
         });
         pt.play();
+
+    }
+    @FXML
+    public void signUp(ActionEvent e1) throws IOException {
+        login.getScene().getWindow().hide();
+        Stage signup=new Stage();
+        Parent root= FXMLLoader.load(getClass().getResource("/FXML/signup.fxml"));
+        Scene scene=new Scene(root);
+        signup.setScene(scene);
+        signup.show();
+        signup.setResizable(false);
 
     }
 
