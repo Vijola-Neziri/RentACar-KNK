4242ad94f18e4fb0910bd8fe71a2925b8534f0e1
RinesaBislimi <118773246+RinesaBislimi@users.noreply.github.com>
2023-05-08 14:57:06 +0200

    Main

---
+++ src/main/java/ConnectionMysql/Configs.java
@@ -0,0 +1,11 @@
+package ConnectionMysql;
+
+public class Configs {
+
+    protected  String dbhost ="localhost";
+    protected String dbport ="3306";
+    protected  String dbuser = "root";
+    protected String dbpass = "Rinesa123.";
+    protected  String  dbname = "KNKRentCar";
+
+}
