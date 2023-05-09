80c5571ebaf64ee261068a2b43030962d578b4b0
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-05-09 22:42:29 +0200

    models

--- src/main/java/app/LoginForm.java
+++ src/main/java/app/LoginForm.java
@@ -3,25 +3,34 @@ package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginForm  extends Application {
+public class LoginForm extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(LoginForm.class.getResource("/views/LogInForm.fxml"));
         Pane pane = fxmlLoader.load();
+
+        Label primeDriveLabel = new Label("PrimeDrive");
+        primeDriveLabel.setStyle("-fx-font-size: 36px; -fx-font-weight: bold; -fx-alignment: center;");
+
+
+
+        pane.getChildren().add(primeDriveLabel);
+        primeDriveLabel.setLayoutX(pane.getWidth() / 2 - primeDriveLabel.getWidth() / 2);
+        primeDriveLabel.setLayoutY(pane.getHeight() / 2 - primeDriveLabel.getHeight() / 2);
+
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Log in");
         primaryStage.show();
-
-
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
--- src/main/java/app/SignupForm.java
+++ src/main/java/app/SignupForm.java
@@ -3,6 +3,7 @@ package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -14,6 +15,16 @@ public class SignupForm extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(LoginForm.class.getResource("/SignUpForm.fxml"));
         Pane pane = fxmlLoader.load();
+
+        Label primeDriveLabel = new Label("PrimeDrive");
+        primeDriveLabel.setStyle("-fx-font-size: 36px; -fx-font-weight: bold;");
+
+
+        pane.getChildren().add(primeDriveLabel);
+        primeDriveLabel.setLayoutX(pane.getWidth() / 2 - primeDriveLabel.getWidth() / 2);
+        primeDriveLabel.setLayoutY(pane.getHeight() / 2 - primeDriveLabel.getHeight() / 2);
+
+
         Scene scene = new Scene(pane,450,500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Sign up");
