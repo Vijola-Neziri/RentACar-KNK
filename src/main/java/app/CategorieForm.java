e1b39b6c209e3e2bf805adfaf153f3a19bc40387
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-05-09 12:50:21 +0200

    CategorieForm

--- src/main/java/app/CategorieForm.java
+++ src/main/java/app/CategorieForm.java
@@ -10,5 +10,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class CategorieForm extends Application {
-  
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginForm.class.getResource("/Categories.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Categories");
+        primaryStage.show();
+
+
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
 }
