7ee65c0ab2f5cf481822641285132f4653482044
RinesaBislimi <118773246+RinesaBislimi@users.noreply.github.com>
2023-05-09 12:28:39 +0200

    Main

---
+++ src/main/java/Controller/CategoriesController.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CategoriesController {
+
+    @FXML
+    private AnchorPane categorie;
+
+    @FXML
+    private Button categorieback;
+
+    @FXML
+    private TextField categorieid;
+    @FXML
+    private Button ctgnext;
+
+    @FXML
+    private TextField pricefordayid;
+
+    @FXML
+    void categoriebackbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void categorienextbutton(ActionEvent event) {
+
+    }
+}
