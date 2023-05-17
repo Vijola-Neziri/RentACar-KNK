1aaddb69f0b6639fd9dc0619ddb8e90d60ebdf9c
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-05-17 02:11:13 +0200

    charts updates

--- src/main/java/Controller/ChartsDemoUIController.java
+++ src/main/java/Controller/ChartsDemoUIController.java
@@ -1,15 +1,26 @@
 package Controller;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.chart.*;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+
+
 
 public class ChartsDemoUIController implements Initializable {
     @FXML
@@ -22,6 +33,49 @@ public class ChartsDemoUIController implements Initializable {
 
     @FXML
     private void handleShowBarChart(ActionEvent event) {
+
+        borderPane.setCenter(buildPieChart());
+
+    }
+    private PieChart buildPieChart() {
+        //Create Data
+        ObservableList<PieChart.Data> pieCharDAta = FXCollections.observableArrayList(
+                new PieChart.Data("Product A", 3000),
+                new PieChart.Data("Product B", 1500),
+                new PieChart.Data("Product C", 100)
+        );
+
+
+        //Create PieChart object
+        PieChart pieChart = new PieChart(pieCharDAta);
+        pieChart.setTitle("Products sold");
+        pieChart.setClockwise(true);
+        pieChart.setLabelLineLength(50);
+        pieChart.setLabelsVisible(true);
+        pieChart.setStartAngle(180);
+
+        //Create the context menu and menu item(s)
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem miSwitchToBarChart = new MenuItem("Switch to Bar Chart");
+        contextMenu.getItems().add(miSwitchToBarChart);
+
+        //Display context menu
+        pieChart.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    contextMenu.show(pieChart, event.getScreenX(), event.getScreenY());
+                }
+            }
+        });
+
+
+        miSwitchToBarChart.setOnAction((ActionEvent actionEvent) -> {
+            borderPane.setCenter(buildBarChart());
+        });
+        return pieChart;
+    }
+    private BarChart buildBarChart(){
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Product");
 
@@ -41,8 +95,7 @@ public class ChartsDemoUIController implements Initializable {
         barChart.getData().add(data);
         barChart.setLegendVisible(false);
 
-        // Add bar chart to BorderPane
-        borderPane.setCenter(barChart);
+        return barChart;
     }
 
     @FXML
@@ -53,6 +106,8 @@ public class ChartsDemoUIController implements Initializable {
                 new PieChart.Data("Product B",1500),
                 new PieChart.Data("Product C",100)
         );
+
+
         //Create PieChart object
         PieChart pieChart=new PieChart(pieCharDAta);
         pieChart.setTitle("Products sold");
@@ -61,16 +116,45 @@ public class ChartsDemoUIController implements Initializable {
         pieChart.setLabelsVisible(true);
         pieChart.setStartAngle(180);
 
+        //Create the context menu and menu item(s)
+        ContextMenu contextMenu=new ContextMenu();
+        MenuItem miSwitchToBarChart=new MenuItem("Switch to Bar Chart");
+        contextMenu.getItems().add(miSwitchToBarChart);
+
+        //Display context menu
+        pieChart.addEventHandler(MouseEvent.MOUSE_CLICKED,new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton()== MouseButton.SECONDARY){
+                    contextMenu.show(pieChart,event.getScreenX(),event.getScreenY());
+                }
+            }
+        });
+
+
+        miSwitchToBarChart.setOnAction((ActionEvent actionEvent)-> {
+            borderPane.setCenter(buildBarChart());
+        } );
+
         borderPane.setCenter(pieChart);
     }
 
     @FXML
     private void handleUpdateData(ActionEvent event) {
-        // Handle the action to update data
+        Node node = borderPane.getCenter();
+
+        if (node instanceof PieChart) {
+            PieChart pc = (PieChart) node;
+
+            pc.getData().get(2).setPieValue(750);
+
+        }
     }
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Initialize the controller
+        borderPane.setCenter(buildBarChart());
     }
 }
