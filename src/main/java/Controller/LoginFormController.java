a17c99ea898e073a4ab0e6535b6a7d38b7398f6b
Rinesa Zuzaku <119458053+RinesaZuzaku@users.noreply.github.com>
2023-05-19 14:57:10 +0200

    Transaltions

--- src/main/java/Controller/LoginFormController.java
+++ src/main/java/Controller/LoginFormController.java
@@ -7,10 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -25,11 +22,18 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LoginFormController implements Initializable {
     @FXML
     private Button loginid;
+
+    @FXML
+    private RadioButton alButton;
+
+    @FXML
+    private RadioButton enButton;
 
     @FXML
     private TextField passwordid;
@@ -42,6 +46,18 @@ public class LoginFormController implements Initializable {
 
     @FXML
     private TextField usernameid;
+
+    @FXML
+    private Label label_clickSignupToRegisterid;
+
+    @FXML
+    private Label label_passwordid;
+
+    @FXML
+    private Label label_selectUserid;
+
+    @FXML
+    private Label label_usernameid;
 
     @FXML
     private ComboBox<String> comboBox;
@@ -63,10 +79,10 @@ public class LoginFormController implements Initializable {
         }
     }
 
-    @FXML
-    public void changeLanguage() {
-
-    }
+//    @FXML
+//    public void changeLanguage() {
+//
+//    }
 
     @FXML
     public void selectUser() {
@@ -76,6 +92,42 @@ public class LoginFormController implements Initializable {
     @FXML
     void loginaction(ActionEvent event) {
         handleLogin();
+    }
+
+
+    public  void changeLanguage() {
+        ToggleGroup languageToggleGroup = new ToggleGroup();
+        alButton.setToggleGroup(languageToggleGroup);
+        enButton.setToggleGroup(languageToggleGroup);
+        languageToggleGroup.selectedToggleProperty().addListener((observable, oldToggle, newToggle) -> {
+            if(newToggle == alButton) {
+                Locale currentLocale = new Locale("sq", "AL");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.AL_SQ", currentLocale);
+                loginid.setText(bundle.getString("login.button.text"));
+                signupid.setText(bundle.getString("signup.button.text"));
+                usernameid.setPromptText(bundle.getString("Enter.your.username.placeholder"));
+                passwordid.setPromptText(bundle.getString("Enter.your.password.placeholder"));
+                label_clickSignupToRegisterid.setText(bundle.getString("Click.signup.to.register.label"));
+                label_selectUserid.setText(bundle.getString("select.user.label"));
+                label_usernameid.setText(bundle.getString("username.label"));
+
+
+
+            }else if(newToggle == enButton)  {
+                Locale currentLocale = new Locale("en", "US");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.US_EN", currentLocale);
+                loginid.setText(bundle.getString("login.button.text"));
+                signupid.setText(bundle.getString("signup.button.text"));
+                usernameid.setPromptText(bundle.getString("Enter.your.username.placeholder"));
+                passwordid.setPromptText(bundle.getString("Enter.your.password.placeholder"));
+                label_clickSignupToRegisterid.setText(bundle.getString("Click.signup.to.register.label"));
+                label_selectUserid.setText(bundle.getString("select.user.label"));
+                label_usernameid.setText(bundle.getString("username.label"));
+
+            }
+
+        });
+        languageToggleGroup.selectToggle(alButton);
     }
 
 
@@ -146,7 +198,12 @@ public class LoginFormController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         comboBox.getItems().addAll("Admin", "User"); // Add options to the comboBox
         handler = new DBHandler();
+        changeLanguage();
+
     }
+    
+
+
 
     private void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
--- src/main/java/Controller/SignupFormController.java
+++ src/main/java/Controller/SignupFormController.java
@@ -13,6 +13,7 @@ import repository.UserRepository;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class SignupFormController implements Initializable {
@@ -43,12 +44,93 @@ public class SignupFormController implements Initializable {
     @FXML
     private TextField usernameid;
 
+
+    @FXML
+    private Label label_addressid;
+
+    @FXML
+    private Label label_doYouHaveAnAccountid;
+
+    @FXML
+    private Label label_genderid;
+
+    @FXML
+    private Label label_nameid;
+
+    @FXML
+    private Label label_passwordid;
+
+    @FXML
+    private Label label_phoneid;
+
+    @FXML
+    private Label label_usernameid;
+
     private DBHandler handler;
     private UserRepository userRepository;
     @FXML
     private Label myLabel;
     @FXML
+    private RadioButton alButton;
+
+    @FXML
+    private RadioButton enButton;
+
+    @FXML
     private RadioButton Male, Female;
+
+    public  void changeLanguage() {
+        ToggleGroup languageToggleGroup = new ToggleGroup();
+        alButton.setToggleGroup(languageToggleGroup);
+        enButton.setToggleGroup(languageToggleGroup);
+        languageToggleGroup.selectedToggleProperty().addListener((observable, oldToggle, newToggle) -> {
+            if(newToggle == alButton) {
+                Locale currentLocale = new Locale("sq", "AL");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.AL_SQ", currentLocale);
+                loginid.setText(bundle.getString("login.button"));
+                signupid.setText(bundle.getString("signup.button"));
+                usernameid.setPromptText(bundle.getString("Enter.your.username.placeholder1"));
+                passwordid.setPromptText(bundle.getString("Enter.your.password.placeholder1"));
+                name.setPromptText(bundle.getString("Enter.your.name.placeholder"));
+                phoneid.setPromptText(bundle.getString("Enter.your.phone.placeholder"));
+                addressid.setPromptText(bundle.getString("Enter.your.address.placeholder"));
+                label_nameid.setText(bundle.getString("Name.label"));
+                label_usernameid.setText(bundle.getString("Username.label"));
+                label_phoneid.setText(bundle.getString("Phone.label"));
+                label_addressid.setText(bundle.getString("Address.label"));
+                label_genderid.setText(bundle.getString("Gender.label"));
+                male.setText(bundle.getString("Male.radio"));
+                female.setText(bundle.getString("Female.radio"));
+                label_doYouHaveAnAccountid.setText(bundle.getString("Do.you.have.an.account.label"));
+
+
+
+
+
+            }else if(newToggle == enButton)  {
+                Locale currentLocale = new Locale("en", "US");
+                ResourceBundle bundle = ResourceBundle.getBundle("translations.US_EN", currentLocale);
+                loginid.setText(bundle.getString("login.button"));
+                signupid.setText(bundle.getString("signup.button"));
+                usernameid.setPromptText(bundle.getString("Enter.your.username.placeholder1"));
+                passwordid.setPromptText(bundle.getString("Enter.your.password.placeholder1"));
+                name.setPromptText(bundle.getString("Enter.your.name.placeholder"));
+                phoneid.setPromptText(bundle.getString("Enter.your.phone.placeholder"));
+                addressid.setPromptText(bundle.getString("Enter.your.address.placeholder"));
+                label_nameid.setText(bundle.getString("Name.label"));
+                label_usernameid.setText(bundle.getString("Username.label"));
+                label_phoneid.setText(bundle.getString("Phone.label"));
+                label_addressid.setText(bundle.getString("Address.label"));
+                label_genderid.setText(bundle.getString("Gender.label"));
+                male.setText(bundle.getString("Male.radio"));
+                female.setText(bundle.getString("Female.radio"));
+                label_doYouHaveAnAccountid.setText(bundle.getString("Do.you.have.an.account.label "));
+
+            }
+
+        });
+        languageToggleGroup.selectToggle(alButton);
+    }
 
     public void getMale(ActionEvent event) {
         if (Male.isSelected()) {
@@ -149,6 +231,7 @@ public class SignupFormController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         signupid.setOnAction(this::signupaction);
         userRepository = new UserRepository();
+        changeLanguage();
     }
 }
 
