6c15f3d603a312ab5d1a45eb1965bd089f9fa1b5
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-05-20 14:24:06 +0200

    customer

---
+++ src/main/java/app/customerForm.java
@@ -0,0 +1,24 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class customerForm extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(app.UserRent1.class.getResource("/views/customer.fxml"));
+        Pane pane = fxmlLoader.load();
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Help");
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
